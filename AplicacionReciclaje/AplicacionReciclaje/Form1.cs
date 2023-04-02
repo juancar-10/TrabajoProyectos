5bfe3a495f56bd1bcdd575245dd4ffb200dfd824
Juan Carlos <juancarlos.garciaventura@gmail.com>
2023-04-02 21:54:53 +0200

    Create Form1.cs

---
+++ AplicacionReciclaje/AplicacionReciclaje/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AplicacionReciclaje
+{
+    public partial class frmRegistrer : Form
+    {
+        public frmRegistrer()
+        {
+            InitializeComponent();
+        }
+
+        private void frmRegistrer_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}

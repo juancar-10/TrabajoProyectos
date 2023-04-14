36ca1ad2fec6a243bd67bae8fb267e84461eaf28
Juan Carlos <juancarlos.garciaventura@gmail.com>
2023-04-14 19:43:25 +0200

    Edición Interfaz Registro

--- AplicacionReciclaje/AplicacionReciclaje/Form1.Designer.cs
+++ AplicacionReciclaje/AplicacionReciclaje/Form1.Designer.cs
@@ -33,9 +33,13 @@
             this.txtUsername = new System.Windows.Forms.TextBox();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.txtPassword = new System.Windows.Forms.Label();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.checkBoxShowPassword = new System.Windows.Forms.TextBox();
             this.txtComPassword = new System.Windows.Forms.Label();
             this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
@@ -91,16 +95,16 @@
             this.txtPassword.Text = "Password";
             this.txtPassword.Click += new System.EventHandler(this.label3_Click);
             // 
-            // textBox2
+            // checkBoxShowPassword
             // 
-            this.textBox2.BackColor = System.Drawing.Color.Gainsboro;
-            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.textBox2.Font = new System.Drawing.Font("MS UI Gothic", 16.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox2.Location = new System.Drawing.Point(79, 618);
-            this.textBox2.Multiline = true;
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(513, 55);
-            this.textBox2.TabIndex = 6;
+            this.checkBoxShowPassword.BackColor = System.Drawing.Color.Gainsboro;
+            this.checkBoxShowPassword.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.checkBoxShowPassword.Font = new System.Drawing.Font("MS UI Gothic", 16.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBoxShowPassword.Location = new System.Drawing.Point(79, 618);
+            this.checkBoxShowPassword.Multiline = true;
+            this.checkBoxShowPassword.Name = "checkBoxShowPassword";
+            this.checkBoxShowPassword.Size = new System.Drawing.Size(513, 55);
+            this.checkBoxShowPassword.TabIndex = 6;
             // 
             // txtComPassword
             // 
@@ -114,12 +118,61 @@
             // checkBox1
             // 
             this.checkBox1.AutoSize = true;
+            this.checkBox1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.checkBox1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.checkBox1.Location = new System.Drawing.Point(293, 693);
             this.checkBox1.Name = "checkBox1";
-            this.checkBox1.Size = new System.Drawing.Size(248, 41);
+            this.checkBox1.Size = new System.Drawing.Size(243, 41);
             this.checkBox1.TabIndex = 7;
             this.checkBox1.Text = "Show Password";
             this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.MediumSlateBlue;
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(82, 761);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(513, 79);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "REGISTER";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.White;
+            this.button2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.ForeColor = System.Drawing.Color.MediumSlateBlue;
+            this.button2.Location = new System.Drawing.Point(82, 889);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(513, 79);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "CLEAR";
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(168, 1001);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(340, 37);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Already Have an Account";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.label4.ForeColor = System.Drawing.Color.MediumSlateBlue;
+            this.label4.Location = new System.Drawing.Point(237, 1069);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(201, 37);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Back to LOGIN";
             // 
             // frmRegistrer
             // 
@@ -127,8 +180,12 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(677, 1279);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.checkBox1);
-            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.checkBoxShowPassword);
             this.Controls.Add(this.txtComPassword);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.txtPassword);
@@ -138,7 +195,7 @@
             this.Font = new System.Drawing.Font("Nirmala UI", 10.125F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.ForeColor = System.Drawing.SystemColors.ButtonShadow;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "frmRegistrer";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
@@ -155,9 +212,13 @@
         private System.Windows.Forms.TextBox txtUsername;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Label txtPassword;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox checkBoxShowPassword;
         private System.Windows.Forms.Label txtComPassword;
         private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
     }
 }
 

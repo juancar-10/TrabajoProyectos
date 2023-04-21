dd283c738f864dad96bf3809a93936e0f0595422
Juan Carlos García <juancarlos.garciaventura@gmail.com>
2023-04-21 19:19:08 +0200

    Añadimos Base de Datos

---
+++ AplicacionReciclaje/AplicacionReciclaje/dbo.Table.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[InfoUsuario]
+(
+	[Id] INT IDENTITY(1,1) NOT NULL PRIMARY KEY, 
+    [NombreUsuario] NVARCHAR(50) NULL, 
+    [Contraseña] NVARCHAR(50) NULL, 
+    [Puntos] NVARCHAR(50) NULL
+)
